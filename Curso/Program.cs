0ab572dd2de536e626eaef0d904ecc61d2f7e960
Junior Denadai <Junior Denadai@DESKTOP-0N42MF1>
2021-01-26 19:28:01 -0500

    criacao de veriaveis de triangulo

---
+++ Curso/Program.cs
@@ -0,0 +1,25 @@
+﻿using Course;
+using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triangulo X");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com as medidas do triangulo Y");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
